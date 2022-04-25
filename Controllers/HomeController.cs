ba894c1f576a9d9b3f55e0e6efc6ef555e3edfb0
Shivam Thorat <shivamthorat4@gmail.com>
2022-04-25 11:17:59 +0530

    footballleague

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Assingment1_FootballLeague.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Assingment1_FootballLeague.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/SaveDataController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Assingment1_FootballLeague.Controllers
+{
+    public class SaveDataController : Controller
+    {
+        public ActionResult SaveData()
+        {
+            ViewBag.result = "";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SaveData(string MatchId, string TeamName1,string TeamName2,string status,string WinningTeam, string Points )
+
+        {
+            String constring = "Data Source=DESKTOP-I74GCFC; Initial Catalog=FootballLeague; Integrated Security=true";
+            SqlConnection sqlcon = new SqlConnection(constring);
+            String Team = "TeamDataInsert"; ;
+            sqlcon.Open();
+            SqlCommand com = new SqlCommand(Team, sqlcon);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@MatchId", Convert.ToInt16(MatchId));
+            com.Parameters.AddWithValue("@TeamName1", TeamName1);
+            com.Parameters.AddWithValue("@TeamName2", TeamName2);
+            com.Parameters.AddWithValue("@status", status);
+            com.Parameters.AddWithValue("@WinningTeam", WinningTeam);
+            com.Parameters.AddWithValue("@Points", Points);
+
+            com.ExecuteNonQuery();
+            sqlcon.Close();
+            ViewBag.result = "Record Has Been Saved Successfully";
+            return View();
+        }
+    }
+}
